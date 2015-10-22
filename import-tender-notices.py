6d80da452a47c434a54ced221d0087bedb7bc54f
David Megginson <david.megginson@megginson.com>
2015-10-22 14:42:40 -0400

    Using class for list of notices as well.

--- import-tender-notices.py
+++ import-tender-notices.py
@@ -3,18 +3,65 @@
 import sys
 import csv
 import re
-import pprint
+import collections
 
+
+class TenderNoticeList(collections.UserDict):
+    """A list of tender notices."""
+
+    def __init__(self):
+        super().__init__()
+
+    def load_notice(self, row):
+        """
+        Import a single notice from a CSV row.
+        @param row The CSV data row to load.
+        """
+        refno = row.get('reference_number')
+        if not refno:
+            raise Exception("No reference number provided: " + str(row))
+        notice = self.data.get(refno)
+        if notice:
+            notice.load(row)
+        else:
+            notice = TenderNotice(row)
+            self.data[refno] = notice
+
+    @staticmethod
+    def load(source):
+        """
+        Load a list of notices from CSV.
+        @param source A filename or input stream for a CSV file.
+        @return a TenderNoticeList object
+        """
+        print("Source is {}".format(source))
+        if hasattr(source, 'read'):
+            return TenderNoticeList._load(source)
+        else:
+            with open(source, 'r', encoding='utf-8-sig') as input:
+                return TenderNoticeList._load(input)
+
+    @staticmethod
+    def _load(input):
+        """Internal load method."""
+        notices = TenderNoticeList()
+        reader = csv.DictReader(input)
+        for row in reader:
+            notices.load_notice(row)
+        return notices
+
+    
 class TenderNotice(object):
+    """A single tender notice"""
 
     def __init__(self, row=None):
         self.gsins = set()
         self.regions_delivery = set()
         self.regions_opportunity = set()
         if row:
-            self.merge(row)
+            self.load(row)
 
-    def merge(self, row):
+    def load(self, row):
         self.reference_number = row['reference_number']
         if row['language'] == 'English':
             self.title_en = row['title']
@@ -31,6 +78,9 @@ class TenderNotice(object):
 
         regions = self._parse_regions(row['region_opportunity'])
         self.regions_opportunity = self.regions_opportunity.union(regions)
+
+    def __str__(self):
+        return "<Tender notice: {} {}>".format(self.reference_number, self.title_en)
 
     def _parse_regions(self, s):
         """Parse comma-separated list of regions."""
@@ -57,16 +107,19 @@ class TenderNotice(object):
             'fr': 'https://achatsetventes.gc.ca/donnees-sur-l-approvisionnement/appels-d-offres/' + id
         }
 
-def import_data(input):
-    """Import the data from a CSV file."""
-    reader = csv.DictReader(input)
-    for row in reader:
-        notice = TenderNotice(row)
-        print(notice)
+    @staticmethod
+    def load_data(input):
+        """Import the data from a CSV file."""
+        notices = TenderNoticeList()
+        reader = csv.DictReader(input)
+        for row in reader:
+            notices.load_notice(row)
+        return notices
 
 if sys.argv[1]:
-    with open(sys.argv[1], 'r', encoding='utf-8-sig') as input:
-        import_data(input)
+    notices = TenderNoticeList.load(sys.argv[1])
 else:
-    import_data(sys.stdin)
+    notices = TenderNoticeList.load(sys.stdin)
 
+for refno in notices:
+    print(notices.get(refno))
